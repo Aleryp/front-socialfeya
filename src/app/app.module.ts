42ee8af6ef66e492556a413be645d16921c68604
AleRyp <andrik.koz@gmail.com>
2020-11-29 03:21:54 +0200

    UPD Login logic. Templates. Fixed some errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import {AuthModule} from '../auth/auth.module';
 import { ProfileModule } from '../profile/profile.module';
 import { NotfoundComponent } from '../shared/components/notfound/notfound.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,8 +18,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     AuthModule,
     ProfileModule,
+    FormsModule,
     BrowserAnimationsModule
   ],
   providers: [],
